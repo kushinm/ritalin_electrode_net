997b51e7ad51760b5e044f45c7c4312d9ff756ed
Kushin <kushinm11@gmail.com>
2019-10-18 16:31:06 -0500

    Fix bugs in cross validation

--- analysis/MATLAB/CSV_generator.m
+++ analysis/MATLAB/CSV_generator.m
@@ -1,4 +1,4 @@
-load("../../data/LFP_Delay_PFC_STR.mat")
+load("../../data/LFP_Delay_PFC_STR.mat");
 data = DelayFP;
 num_elecs = [];
 for i=1:8
@@ -10,15 +10,24 @@ for i=1:8
          base = cat(3,base, data(i).PFC_base_delay(:,j,:));
          drug = cat(3,drug, data(i).PFC_drug_delay(:,j,:));
     end
-    acc = data(i).BaseCov(:,1);
-    turn = data(i).BaseCov(:,2);
+    base_acc = data(i).BaseCov(:,1);
+    base_turn = data(i).BaseCov(:,2);
+    drug_acc = data(i).drugCov(:,1);
+    drug_turn = data(i).drugCov(:,2);
     basename ="../../data/"+"rat"+i+"base";
     writematrix(base,basename+".csv");
     drugname = "../../data/"+"rat"+i+"drug";
     writematrix(drug,drugname+".csv");
     accname = "../../data/"+"rat"+i+"acc";
-    writematrix(acc,accname+".csv");
+   
+    writematrix(base_acc,accname+"base"+".csv");
+    writematrix(drug_acc,accname+"drug"+".csv");
+    
     turnname = "../../data/"+"rat"+i+"turn";
-    writematrix(turn,turnname+".csv");
+    
+    
+    writematrix(base_turn,turnname+"base"+".csv");
+    writematrix(drug_turn,turnname+"drug"+".csv");
+    
 end
 writematrix(num_elecs,"../../data/num_electrodes.csv");
